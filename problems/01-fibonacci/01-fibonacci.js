409bbd93a140145c46c239eebb833059ae1c0ef0
FunderJM <jer.fun1@gmail.com>
2019-04-02 18:32:42 -0400

    message

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -2,5 +2,15 @@
 Define function: fib(n) 
 Return the nth number in the fibonacci sequence. */
 function fib(n) {
-
+    if(n == 0){
+        return 0;
+    }
+    if(n ==1){
+        return 1;
+    }else{
+        return fib(n-1) + fib(n-2);
+    }
 }
+
+let fi = fib(20);
+console.log(fi);
