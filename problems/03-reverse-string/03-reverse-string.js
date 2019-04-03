6c062f69a53dd9fd9fcff8488271d44ac5bbed72
FunderJM <jer.fun1@gmail.com>
2019-04-03 17:41:50 -0400

    Reverse String

--- problems/03-reverse-string/03-reverse-string.js
+++ problems/03-reverse-string/03-reverse-string.js
@@ -2,5 +2,16 @@
 Define function: reverseStr(someStr)
 Reverse and return the String. */
 function reverseStr(someStr) {
-  
+    let funArr = someStr.split('');
+    let funArr2 = [];
+
+    for(let n = someStr.length - 1; n >= 0; n--){
+        funArr2.push(funArr[n]);
+    }
+    return funArr2;
 }
+
+console.log(reverseStr('Hello world!'));
+console.log(reverseStr('This is a test.'));
+console.log(reverseStr('As is this.'));
+console.log(reverseStr('I think it is working'));
--- problems/04-factorial/04-factorial.js
+++ problems/04-factorial/04-factorial.js
@@ -2,5 +2,17 @@
 Define function: factorial(someNum)
 Use recursion to compute and return the factorial of someNum.  */
 function factorial(sumNum) {
-  
+    //let n = sumNum;
+    //let fac = 1;
+
+    //for(n; n > 0; n--){
+        //fac = fac * n;
+    //}
+
+    //console.log(fac);
+    
 }
+
+let num = 7;
+
+console.log(factorial(num));
