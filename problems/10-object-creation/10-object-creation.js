ad3a209d2192c86bdff692c417e6e5830898c1ba
FunderJM <jer.fun1@gmail.com>
2019-04-04 18:37:43 -0400

    Object Creation

--- problems/10-object-creation/10-object-creation.js
+++ problems/10-object-creation/10-object-creation.js
@@ -1,3 +1,20 @@
 /* 10 Object Creation
 Create an object using an object literal, constructor function, and es6 class.
 The object should have the fields name and age.*/
+
+let osVersion = {
+    name: 'Windows 7',
+    age: '9 '
+}
+
+class OS{
+    constructor(name,age){
+        this.name = name;
+        this.age = age;
+    }
+}
+
+let myOS = new OS('Windows 10','3');
+
+console.log('This is the best OS : ', osVersion);
+console.log('This is my current OS : ', myOS);
