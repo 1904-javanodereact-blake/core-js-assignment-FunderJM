7ea535826dcc1055eebd70802ae51702b2434646
FunderJM <jer.fun1@gmail.com>
2019-04-04 17:50:06 -0400

    Factorial Recursion

--- problems/04-factorial/04-factorial.js
+++ problems/04-factorial/04-factorial.js
@@ -10,9 +10,21 @@ function factorial(sumNum) {
     //}
 
     //console.log(fac);
+    let n = 1;
+    let m = sumNum
+
+    if(m>0){
+        n = m * factorial(m-1);
+        return n;
+    }else{
+        return n;
+    }
     
 }
 
-let num = 7;
-
-console.log(factorial(num));
+console.log(`The factorial of 7 is: ${factorial(7)}`);
+console.log(`The factorial of 6 is: ${factorial(6)}`);
+console.log(`The factorial of 5 is: ${factorial(5)}`);
+console.log(`The factorial of 4 is: ${factorial(4)}`);
+console.log(`The factorial of 3 is: ${factorial(3)}`);
+console.log(`The factorial of 10 is: ${factorial(10)}`);
