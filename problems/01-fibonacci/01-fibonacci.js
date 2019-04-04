4cc7983bbe49731915a728f00d7490f6ae9995eb
FunderJM <jer.fun1@gmail.com>
2019-04-04 18:51:32 -0400

    Added more examples to the fibonacci program

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -3,18 +3,17 @@ Define function: fib(n)
 Return the nth number in the fibonacci sequence. */
 function fib(n) {
     if(n == 0){
-        // If n is equal to 0, there is no need to run any more functions.
         return 0;
     }
     if(n ==1){
-        // If n is equal to 1, when put in an array. You would get the number 1.
         return 1;
     }else{
-        // By running the function within the function, it forces the program to run the sequence from a
-        // top-down perspective.
         return fib(n-1) + fib(n-2);
     }
 }
 
-let fi = fib(7); // where I assign the value of n.
-console.log(fi);
+
+console.log(`The 7th number in the fibonacci sequense is : ${fib(7)}`);
+console.log(`The 13th number in the fibonacci sequense is : ${fib(13)}`);
+console.log(`The 24th number in the fibonacci sequense is : ${fib(24)}`);
+console.log(`The 4th number in the fibonacci sequense is : ${fib(4)}`);
